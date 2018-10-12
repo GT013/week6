4be1dc0ad78f51ef529e6f0d773836521a7c077c
nantikan <60030049@kmitl.ac.th>
2018-10-12 20:45:07 +0700

    fac

--- week6/week6/Program.cs
+++ week6/week6/Program.cs
@@ -10,12 +10,17 @@ namespace week6
     {
         static void Main(string[] args)
         {
-            sayHello();
+            long c = Factorial(5);
+            Console.WriteLine("Factorial of 5 = {0}", c);
             Console.ReadLine();
         }
-        static void sayHello()
+
+        static long Factorial(int n)
         {
-            Console.WriteLine("Hello World!");
+            if (n == 0)  // Factorial 0 = 1
+                return 1;
+            return n * Factorial(n - 1);
         }
+
     }
 }
